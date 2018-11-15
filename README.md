1ff7bf7e30d4433724b4a9e40ddea8fee6289514
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-11-15 23:17:32 +0700

    push files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# docker-php7phalcon
+Getting started for Docker Engine CE (Free Version)
+---------------------------------------------------
+
+```
+sudo mkdir -p build && cd build
+
+sudo apt-get install git -y
+
+sudo git clone https://github.com/drivesoft-technology/docker-awesome.git
+
+cd docker-awesome
+```
+
+
+Install Docker Engine CE v18.06.1 (Free Version)
+---------------------------------------------------
+
+```
+bash docker-install/install-docker-engine-on-ubuntu18.sh
+```
+
+
+Install Docker Compose v1.22.0
+---------------------------------------------------
+
+```
+bash docker-install/install-docker-compose-on-ubuntu18.sh
+```
+
+
+Start Service | Phalcon Application Template
+---------------------------------------------------
+
+```
+sudo git clone https://github.com/codestepz/docker-php7phalcon.git
+sudo docker-compose -f docker-php7phalcon/docker-compose.yml up -d
+```
+
+```
+http://[IP ADDRESS] << Nginx v1.15.4-alpine
+
+http://[IP ADDRESS]/info.php << PHP v7.2.10 (FPM) 
+```
---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title> Phalcon Framework 3.4.1 </title>
+        <link rel="shortcut icon" href="http://localhost/images/favicon/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="http://localhost/images/favicon/favicon.ico" type="image/x-icon">
+        <base href="http://localhost" />
+        <script>var baseUrl = "http://localhost";</script>
+        <link rel="stylesheet" type="text/css" href="http://localhost/themes/main/assets/style/theme-build.css?v=1.0.0" />
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="container">
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="text-welcome" style="padding-top: 230px;">
+                                    <h1 style="margin:0;padding:0;font-size:4.0em;"> Phalcon Framework </h1>
+                                    <h2 style="margin:0;padding: 15px 0 0;font-size:1.6em;"> CodeStep โค้ดสเต็ป - เรียนรู้ออนไลน์ สะดวก ง่าย เข้าถึงได้ทุกที่ </h2>
+                                </div>
+                            </div>
+                        </div>      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
