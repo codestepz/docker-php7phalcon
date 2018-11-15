481bcaeb4cb943353c43bfa78c8b2c6fde1159c3
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-11-15 23:21:53 +0700

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title> Phalcon Framework 3.4.1 </title>
+        <title> Phalcon Framework v3.4.1 </title>
         <link rel="shortcut icon" href="http://localhost/images/favicon/favicon.ico" type="image/x-icon">
         <link rel="icon" href="http://localhost/images/favicon/favicon.ico" type="image/x-icon">
         <base href="http://localhost" />
@@ -19,7 +19,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="text-welcome" style="padding-top: 230px;">
-                                    <h1 style="margin:0;padding:0;font-size:4.0em;"> Phalcon Framework </h1>
+                                    <h1 style="margin:0;padding:0;font-size:4.0em;"> Phalcon Framework v3.4.1 </h1>
                                     <h2 style="margin:0;padding: 15px 0 0;font-size:1.6em;"> CodeStep โค้ดสเต็ป - เรียนรู้ออนไลน์ สะดวก ง่าย เข้าถึงได้ทุกที่ </h2>
                                 </div>
                             </div>
